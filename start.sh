81449a8ceea91662790707fc029db9231cdbed0a
Sanjivinsomke07 <165073449+Sanjivinsomke07@users.noreply.github.com>
2024-03-26 23:50:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Advance-File-Share-Bot
 fi
-cd /url-Shortner-
+cd /Advance-File-Share-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
